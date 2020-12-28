28d3d46dfb940bd9f4aac9d2b5ccb815a7b0a6db
omd24 <74592722+omd24@users.noreply.github.com>
2020-12-28 11:31:28 +0330

    Update fxc_compile.bat

--- win32_box/shaders/fxc_compile.bat
+++ win32_box/shaders/fxc_compile.bat
@@ -1,1 +1,1 @@
-fxc.exe /T vs_5_0 /E VertexShader_Main cbuffer_shader.hlsl /Fo vs.bin && fxc.exe /T ps_5_0 /E PixelShader_Main cbuffer_shader.hlsl /Fo ps.bin
+fxc.exe /T vs_5_0 /E VertexShader_Main box_shader.hlsl /Fo vs.bin && fxc.exe /T ps_5_0 /E PixelShader_Main box_shader.hlsl /Fo ps.bin
